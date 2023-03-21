c58876e50ca38c2d5841b8801276db155f17cf5e
PCB <perbjester@gmail.com>
2023-03-21 22:20:56 +0100

    stuff

---
+++ src/routes/authRoutes.ts
@@ -0,0 +1,27 @@
+import jwt from 'jsonwebtoken';
+import { Router } from 'express';
+import bcrypt from 'bcrypt';
+import dotenv from 'dotenv';
+dotenv.config();
+const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD || '';
+
+export const router = Router();
+
+router.post('/login', (req, res) => {
+  const valid = bcrypt.compareSync(req.body.password, ADMIN_PASSWORD);
+  console.log('VALID?', valid);
+  if (!valid) return res.sendStatus(401);
+
+  const token = jwt.sign(
+    {
+      id: 'PCB',
+    },
+    process.env.ACCESS_TOKEN_SECRET || '',
+    { expiresIn: '8h' }
+  );
+
+  res.send({
+    success: true,
+    token: token,
+  });
+});
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,30 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import logger from 'morgan';
+import cors from 'cors';
 
 dotenv.config();
-
-const app: Express = express();
 const port = process.env.PORT;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
+const app: Express = express();
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+  })
+);
+
+app.get('/status', (req: Request, res: Response) => {
+  res.setHeader('Content-Type', 'text/html');
+  res.send(`<h1>Server running!</h1>`);
 });
 
+//ROUTES
+//app.use('/api/auth', authRoutes);
+
 app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+  console.log(`⚡️ Server is running at http://localhost:${port}`);
 });
