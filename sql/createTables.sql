d52939217b66564bcf5c1ed883d0618b3300c273
PCB <perbjester@gmail.com>
2023-03-21 21:27:51 +0100

    initial code

---
+++ sql/createTables.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS `items`;
+
+CREATE TABLE
+    `items` (
+        `id` int unsigned NOT NULL AUTO_INCREMENT,
+        `title` varchar(255) NOT NULL,
+        `imageURL` varchar(255) DEFAULT NULL,
+        `creator` varchar(100) DEFAULT NULL,
+        `averageRating` double DEFAULT NULL,
+        `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        PRIMARY KEY (`id`)
+    ) ENGINE = InnoDB AUTO_INCREMENT = 61 DEFAULT CHARSET = utf8mb3;
